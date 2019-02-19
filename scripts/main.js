24ad4738518850a29d0a85ec40c5ddadaf368db0
mdnrosen <mdnrosen@hotmail.co.uk>
2019-02-19 12:26:42 +0000

    user ship buttons in place. cpu place fleet function installed

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,21 @@
 $(() => {
+
   // VARIABLES TO MANIPULATE FOR CLICKEVENT
   const textBox = document.querySelector('.textbox')
   const startButton = document.querySelector('.start')
   const mainGame = document.querySelector('.maingame')
+
+  // MAKE THE START BUTTON DISAPPEAR AND STUFF
+  startButton.addEventListener('click', () => {
+    startButton.style.display = 'none'
+    mainGame.style.display = 'flex'
+    textBox.innerHTML = 'Welcome to BATTLESHIPS'
+  })
+
   const mapSize = new Array(100)
   const radarSize = new Array(100)
+  const playerMap = []
+  const cpuMap = []
   const usedMap = []
   const usedRadar = []
   const occupiedMap = []
@@ -17,6 +28,7 @@ $(() => {
       const grid = document.createElement('div')
       grid.setAttribute('class','grid-item')
       map.appendChild(grid)
+      playerMap.push(map)
     }
   }
   makeMap()
@@ -26,23 +38,12 @@ $(() => {
       const radarGrid = document.createElement('div')
       radarGrid.setAttribute('class','radar-item')
       radar.appendChild(radarGrid)
+      cpuMap.push(radar)
     }
   }
   makeRadar()
 
-
-
-
-
-
-  // MAKE THE START BUTTON DISAPPEAR AND STUFF
-  startButton.addEventListener('click', () => {
-    startButton.style.display = 'none'
-    mainGame.style.display = 'flex'
-    textBox.innerHTML = 'Hello'
-  })
-
-  // SHIP CREATION
+  // ------------------------------SHIP CREATION-------------------------------------------------
   class Ship {
     constructor(shipType,hitPoints, shipLength, shipSunk) {
       this.shipType = shipType,
@@ -66,7 +67,17 @@ $(() => {
   const submarine = new Ship('submarine',3,3,false)
   const destroyer = new Ship('destroyer',2,2,false)
 
-  // *********************************************************
+  const myCarrier = new Ship('carrier',5,5,false)
+  const myBattleship = new Ship('battleship',4,4,false)
+  const myCruiser = new Ship('cruiser',3,3,false)
+  const mySubmarine = new Ship('submarine',3,3,false)
+  const myDestroyer = new Ship('destroyer',2,2,false)
+
+
+  //------------------------------------------------------------------------------------------------
+
+  //$$$$$$$$$$$$$$$$$$$$$$$$$$$$ - CPU SHIP PLACEMENT - $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
   function placeShip(shipType) {
     if (shipType.horizVert() === 1) {
       let vaPoint = shipType.vaShip()
@@ -86,30 +97,69 @@ $(() => {
       //stop occupied squares being used
     }
   }
-  placeShip(carrier)
-  placeShip(battleship)
-  placeShip(cruiser)
-  placeShip(submarine)
-  placeShip(destroyer)
 
-  console.log(radarSize)
-  console.log(occupiedRadar)
+  const cpuPlaceShips = document.querySelector('.cpuPlaceShips')
+  cpuPlaceShips.addEventListener('click', () => {
+    placeShip(carrier)
+    placeShip(battleship)
+    placeShip(cruiser)
+    placeShip(submarine)
+    placeShip(destroyer)
+    cpuPlaceShips.style.visibility = 'hidden'
+    console.log(radarSize)
+  })
+  // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  //************************************PLAYER SHIPS PLACEMENT*****************************************
+  const userCarrier = document.querySelector('.carrier')
+  const userBattleship = document.querySelector('.battleship')
+  const userCruiser = document.querySelector('.cruiser')
+  const userSubmarine = document.querySelector('.submarine')
+  const userDestroyer = document.querySelector('.destroyer')
+  
 
-  // *****************************************************************
-
-  //----------------------------------------------SHOOTING ----------------------------------------------
-  const randomShot = function() {
-    return Math.floor((Math.random() * 100))
+  function placeMyShip(shipType) {
+    if (//user choses vertical//) {
+      let vaPoint = shipType.vaShip()
+      radarSize[vaPoint] = shipType
+      for (let i = 1; i < shipType.shipLength; i++) {
+        vaPoint += 10
+        radarSize[vaPoint] = shipType
+      } occupiedRadar.push(shipType)
+    } else if (//user choses horizontal) > shipType.shipLength) {
+      let haPoint = shipType.haShip()
+      for (let i = 0; i < shipType.shipLength; i++) {
+        haPoint ++
+        radarSize[haPoint] = shipType
+      } occupiedRadar.push(shipType)
+    }
   }
-  const huntShot = function () {
-    return (previousShot + 1 || -1 || + 10 || -10)
-  }
+
+
+  // ************************************************************************************************
+
+  //############################################ - SHOOTING - ############################################
+  const cpuFire = document.querySelector('.cpufire')
+  cpuFire.addEventListener('click', () => {
+    const mapTarget = Math.floor((Math.random() * 100))
+    mapSize[mapTarget].style.background = 'red'
+
+    console.log('fire captain!')
+  })
+
+
+
+  // const randomShot = function() {
+  //   return Math.floor((Math.random() * 100))
+  // }
+  // const huntShot = function () {
+  //   return (previousShot + 1 || -1 || + 10 || -10)
+  // }
 
 
 
 
 
-
+  //########################################################################################
 
   // **************************************DO NOT WRITE BELOW THIS LINE*********************************************************
 })
